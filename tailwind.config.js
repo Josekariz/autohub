e6974c68bfaa51f167573fac8427e86c6b509d2f
Josekariz <sejokarizz@gmail.com>
2026-01-20 16:22:58 +0300

    update tailwind config to extend theme colors for main, card, and accent

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  
   darkMode: "class",
   content: [
-    "./App.*",
     "./app/**/*.{js,jsx,ts,tsx}",
     "./components/**/*.{js,jsx,ts,tsx}",
     "./screens/**/*.{js,jsx,ts,tsx}",
   ],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        main: {
+          light: "#f9fafb", // gray-50
+          dark: "#111827", // gray-900
+        },
+        card: {
+          light: "#ffffff", // white
+          dark: "#1f2937", // gray-800
+        },
+        accent: "#2d7cd0", // blue
+      },
+    },
   },
   plugins: [],
 };
